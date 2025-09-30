0f2aabeaca56d6815217087cf478f2064f214bd9
jhaveriharshil7-lab <jhaveriharshil7@gmail.com>
2025-09-30 10:27:48 +0530

    Add files via upload

---
+++ array17.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+    {
+    int arr[100], n, i;
+    printf("Enter number of elements in array: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    if(n==0){
+    printf("Array is empty");
+    }
+    else{
+    n--;}
+        printf("Array after deletion:\n");
+        for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    getch();
+    }
